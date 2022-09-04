1a385bee5e35bd2a1ee01a21cdd1959fb04861f6
joy <joycseru@gmail.com>
2022-09-04 12:15:33 +0600

    lesson-2

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,7 @@
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Footer from "./Footer";
 import Nav from "./Nav";
 
 function App() {
@@ -9,6 +11,15 @@ function App() {
         <Nav />
         <h1>E-Dashboard</h1>
       </BrowserRouter>
+      {/* <Routes>
+        <Route path="/" element={<h1>Product Component</h1>} />
+        <Route path="/add" element={<h1> Add Product Component</h1>} />
+        <Route path="/update" element={<h1>Update Product Component</h1>} />
+        <Route path="/logout" element={<h1> Logout Component</h1>} />
+        <Route path="/profile" element={<h1> Profile Component</h1>} />
+      </Routes> */}
+
+      <Footer />
     </div>
   );
 }
--- front-end/src/Nav.js
+++ front-end/src/Nav.js
@@ -1,12 +1,29 @@
+// import React from "react";
 import React from "react";
+import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <div>
-      {/* <ul>
+      <ul className="nav-ul">
         <li>
-          <Link to="/">Products</Link>
+          <Link to="">Products</Link>
         </li>
-      </ul> */}
+        <li>
+          <Link to="/add">Add Products</Link>
+        </li>
+        <li>
+          <Link to="/update">Update Products</Link>
+        </li>
+        <li>
+          <Link to="/logout">logout</Link>
+        </li>
+        <li>
+          <Link to="/profile">profile</Link>
+        </li>
+        <li>
+          <Link to="/signup">SignUp</Link>
+        </li>
+      </ul>
       <h1>Header</h1>
     </div>
   );
