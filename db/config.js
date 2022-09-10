9fd43cd4329f917e34b499f4af737c5bab02d8dc
joy <joycseru@gmail.com>
2022-09-10 14:20:07 +0600

    work with signup issue

---
+++ db/config.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://joycseru:f01765711177@x-store.syqjyel.mongodb.net/?retryWrites=true&w=majority"
+);
+
+
--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose")
+require("./db/config");
+const User = require('./db/User') ; 
+const cors = require('cors');
+
+
+
 const app = express();
-app.get("/", (req, resp) => {
-  resp.send("app is working");
+app.use(express.json()) ; 
+app.use(cors()) ; 
+
+app.use(express.json()) ;
+app.get('/',async(req,resp)=>{
+   
+  resp.send('api is on work');
+})  ; 
+app.post('/register', async(req, resp) => {
+ let user = new User(req.body) ;
+ let result =await user.save() ;  
+  resp.send(result);
 });
+
+
+
+
 app.listen(5000);
